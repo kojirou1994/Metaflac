bab2e5705c466a792a99cc58b420eeb45adc1d2f
kojirou <kojirouhtc@gmail.com>
2019-09-03 00:22:56 +0800

    update deps

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/kojirou1994/Kwift.git", from: "0.3.1"),
-        .package(url: "https://github.com/kojirou1994/URLFileManager.git", .branch("master")),
+        .package(url: "https://github.com/kojirou1994/URLFileManager.git", from: "0.0.1"),
         .package(url: "https://github.com/pointfreeco/swift-nonempty.git", from: "0.2.0")
     ],
     targets: [
